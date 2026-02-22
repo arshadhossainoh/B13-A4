a97e5dcfa0cf26940e4fd8d88eb6f07562f8c098
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-02-22 11:16:38 -0500

    worked on toggleing across buttons

--- script/index.js
+++ script/index.js
@@ -1,21 +1,14 @@
 //show selected tab
-function showOnly(id) {
-  // const all = document.getElementById("add-money");
-  // const cashout = document.getElementById("cashout");
-  // const history = document.getElementById("history");
 
-  const all = document.getElementById("all");
+function showOnly(id) {
+  const jobBoard = document.getElementById("job-board");
   const interview = document.getElementById("interview");
   const rejected = document.getElementById("rejected");
 
-  all.classList.add("hidden");
+  //hide eveeryone
+  jobBoard.classList.add("hidden");
   interview.classList.add("hidden");
   rejected.classList.add("hidden");
-
-  //hide eveeryone
-  addmoney.classList.add("hidden");
-  cashout.classList.add("hidden");
-  cashout.classList.add("hidden");
 
   //only show id wala
   const selected = document.getElementById(id);
