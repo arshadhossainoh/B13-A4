a9cddfd161700f0c8b855ca8b25a03f3cdbc1f0f
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-02-22 23:53:07 -0500

    worked on delete functionality

--- script/index.js
+++ script/index.js
@@ -14,3 +14,24 @@ function showOnly(id) {
   const selected = document.getElementById(id);
   selected.classList.remove("hidden");
 }
+
+// delete job posting
+function deleteJob(id) {
+  // const jobBoard = document.getElementById("job-board");
+  // const delteteBtn = document.getElementById("del");
+
+  // const selected = document.getElementById(id);
+  // id.parentNode.removeChild(selected);
+
+  // const childElement = document.getElementById(id);
+  // childElement.parentNode.removeChild(childElement);
+  let jobPost = document.getElementById(id);
+  jobPost.innerHTML = "";
+  totalJobs.innerText = jobs.length--;
+  jobCount.innerText = `${jobs.length} jobs`;
+}
+
+// const delteteBtn = document.getElementById("del");
+// delteteBtn.addEventListener("click", function () {
+//   console.log("btn clicked");
+// });
