57ad87edd9b95707af979a33919f0a131bf5b9e8
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-02-24 01:05:41 -0500

    worked on respective job reflecting number upon switiching buttons

--- script/job.js
+++ script/job.js
@@ -1,9 +1,12 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = "all";
 
 let total = document.getElementById("total-count");
 let interviewCount = document.getElementById("interview-count");
 let rejectedCount = document.getElementById("rejected-count");
+
+let jobCount = document.getElementById("job-count");
 
 const allJobs = document.getElementById("alljobs");
 
@@ -41,17 +44,26 @@ function toggleStyle(id) {
   rejectedFilterBtn.classList.remove("bg-white", "text-black");
 
   const selected = document.getElementById(id);
+  currentStatus = id;
 
-  // remove newing applying clore and setting new
+  // remove newly applying color and setting new
   selected.classList.remove("bg-gray-300", "text-black");
   selected.classList.add("bg-blue-600", "text-black");
 
   if (id == "interview-filter-btn") {
     allJobs.classList.add("hidden");
     filterSection.classList.remove("hidden");
+    renderInterview();
+    jobCount.innerText = `${interviewList.length} jobs`;
   } else if (id == "all-filter-btn") {
     allJobs.classList.remove("hidden");
     filterSection.classList.add("hidden");
+    jobCount.innerText = `${allJobs.children.length} jobs`;
+  } else if (id == "rejected-filter-btn") {
+    allJobs.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    renderRejected();
+    jobCount.innerText = `${rejectedList.length} jobs`;
   }
 }
 
@@ -76,13 +88,57 @@ mainContainer.addEventListener("click", function (e) {
       jobNotes,
     };
 
-    const interviewExist = interviewList.find(
+    const companyExist = interviewList.find(
       (item) => item.companyName == jobInfo.companyName,
     );
-    if (!interviewExist) {
+    if (!companyExist) {
       interviewList.push(jobInfo);
     }
-    renderInterview();
+
+    rejectedList = rejectedList.filter(
+      (item) => item.companyName != jobInfo.companyName,
+    );
+
+    CalCount();
+    if (currentStatus == "rejected-filter-btn") {
+      renderRejected();
+    }
+    // renderInterview();
+  } else if (e.target.classList.contains("rejected-btn")) {
+    const parentNode = e.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const jobTitle = parentNode.querySelector(".job-title").innerText;
+    const jobPerks = parentNode.querySelector(".perks").innerText;
+    const jobStatus = parentNode.querySelector(".status").innerText;
+    const jobNotes = parentNode.querySelector(".notes").innerText;
+
+    parentNode.querySelector(".status").innerText = "Rejected";
+
+    const jobInfo = {
+      companyName,
+      jobTitle,
+      jobPerks,
+      jobStatus,
+      jobNotes,
+    };
+
+    const companyExist = rejectedList.find(
+      (item) => item.companyName == jobInfo.companyName,
+    );
+    if (!companyExist) {
+      rejectedList.push(jobInfo);
+    }
+
+    interviewList = interviewList.filter(
+      (item) => item.companyName != jobInfo.companyName,
+    );
+
+    if (currentStatus == "interview-filter-btn") {
+      renderInterview();
+    }
+
+    CalCount();
+    // renderRejected();
   }
 });
 
@@ -128,4 +184,64 @@ function renderInterview() {
     `;
     filterSection.appendChild(div);
   }
+}
+
+function renderRejected() {
+  filterSection.innerHTML = "";
+
+  for (let reject of rejectedList) {
+    console.log(reject);
+    let div = document.createElement("div");
+    div.className = "card border border-gray-200 ";
+    div.innerHTML = `
+      <div class="card-left">
+            <h3 class="company-name">${reject.companyName}</h3>
+            <p class="job-title">${reject.jobTitle}</p>
+            <br />
+            <p class="perks">${reject.jobPerks}</p>
+
+            <br />
+            <!-- status  -->
+            <p class="status">${reject.jobStatus}</p>
+            <!-- <button id="status-btn" class="border border-gray-300 p-1">
+              NOT APPLIED
+            </button> -->
+            <p class="mb-5 notes">
+              Build cross-platform mobile applications using React Native. Work
+              on products used by millions of users worldwide.
+            </p>
+
+            <button
+              id="interview-btn"
+              class="btn btn-outline btn-success mr-3 interview-btn interview-btn"
+            >
+              INTERVIEW
+            </button>
+            <button
+              id="rejected-btn"
+              class="btn btn-outline btn-error rejected-btn"
+            >
+              REJECTED
+            </button>
+          </div>
+          
+    `;
+    filterSection.appendChild(div);
+  }
+}
+
+jobCount.innerText = `${allJobs.children.length} jobs`;
+
+// delete job posting
+function deleteJob(id) {
+  // const jobBoard = document.getElementById("job-board");
+
+  // id.parentNode.removeChild(selected);
+  // const childElement = document.getElementById(id);
+  // childElement.parentNode.removeChild(childElement);
+  let jobPost = document.getElementById(id);
+  jobPost.innerHTML = "";
+  jobPost.style.border = "none";
+  totalJobs.innerText = jobs.length--;
+  jobCount.innerText = `${jobs.length} jobs`;
 }
